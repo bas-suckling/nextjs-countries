3938dff636e641aaa5209ab54a36ea4eb779bd1f
bas-suckling <sebastian.suckling@compono.com>
2023-06-27 20:55:19 +1200

    remove existing styling, add chakra ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>This is a blank next.js project deployed to vercel</h1>
+      <Text fontSize={32} textAlign={["center"]}>
+        This is a blank next.js project deployed to vercel
+      </Text>
     </main>
   );
 }
