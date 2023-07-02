66712a351e73cf88fc43baa5cd661bb18cd4f71f
bas-suckling <sebastian.suckling@compono.com>
2023-07-02 12:00:01 +1200

    setup next-auth with hardcoded admin creds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,36 @@
-"use client";
-
-import styles from "./page.module.css";
 import {
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  Divider,
-  FormControl,
-  FormLabel,
-  Heading,
-  HStack,
-  Input,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { OAuthButtonGroup } from "./components/oAuthButtonGroup";
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "./components/buttons.component";
+import { User } from "./components/user.component";
 
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 import { __DEV__ } from "@apollo/client/utilities/globals";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
 
-if (__DEV__) {
-  // Adds messages only in a dev environment
-  loadDevMessages();
-  loadErrorMessages();
-}
-
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
-    <main className={styles.main}>
-      <Container
-        maxW="lg"
-        py={{ base: "12", md: "24" }}
-        px={{ base: "0", sm: "8" }}
-      >
-        <Stack spacing="8">
-          <Stack spacing="6">
-            <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
-              <Heading size={{ base: "xs", md: "sm" }}>
-                Welcome to this strange application.
-              </Heading>
-            </Stack>
-          </Stack>
-          <Box
-            py={{ base: "0", sm: "8" }}
-            px={{ base: "4", sm: "10" }}
-            bg={{ base: "transparent", sm: "bg.surface" }}
-            boxShadow={{ base: "none", sm: "md" }}
-            borderRadius={{ base: "none", sm: "xl" }}
-          >
-            <Stack spacing="6">
-              <Stack spacing="6">
-                <Button variant="primary">Sign in</Button>
-                <OAuthButtonGroup />
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Container>
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+      }}
+    >
+      <div>
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
+        <h1>Server Session</h1>
+        <pre>{JSON.stringify(session)}</pre>
+
+        <User />
+      </div>
     </main>
   );
 }
